661c3bb021a68e565282e6d216ab23f6a2e8cb82
Paul Bredenberg <paulbredenberg@gmail.com>
2018-09-11 18:41:13 -0400

    update styles, add interval polling

--- src/app/leaderboard/leaderboard.component.ts
+++ src/app/leaderboard/leaderboard.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ScoreService } from '../services/score.service';
+import { TimerObservable } from 'rxjs/observable/TimerObservable';
+import 'rxjs/add/operator/takeWhile';
+
 
 @Component({
   selector: 'app-leaderboard',
@@ -11,15 +14,28 @@ export class LeaderboardComponent implements OnInit {
 
   public rankings: any[] = [];
 
+  private _pollTime = 10000;
+
   constructor(private _scoreService: ScoreService) { }
 
-  ngOnInit() {
+  private _updateScores() {
     this._scoreService
       .GetTopScores()
       .subscribe(
         (scores: any[]) => {
           console.log('scores', scores);
           this.rankings = scores;
+        }
+      );
+  }
+
+  ngOnInit() {
+    this._updateScores();
+    // Update the scores every _pollTime.
+    TimerObservable.create(0, this._pollTime)
+      .subscribe(
+        () => {
+          this._updateScores();
         }
       );
   }
